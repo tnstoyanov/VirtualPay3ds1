ed2a2c97f80e61c45b501f2ee1b8004b2e249f3a
tnstoyanov <tnstoyanov@gmail.com>
2022-06-22 15:29:01 +0300

    add checksum calc

--- VirtualPay3ds1/Program.cs
+++ VirtualPay3ds1/Program.cs
@@ -1,33 +1,39 @@
-﻿namespace VirtualPay3dsAuth
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace VirtualPay3dsAuth
 {
     class VirtualPay3dsAuthRequestBuilder
     {
         static void Main(string[] args)
         {
             //Your API user provided by VirtualPay
-            string merchantID = "Securcapsecurities";
+            string merchantID = "Securcap3";
             //[TradeNetworks].[Live].[CreditCardDepositCommunicationLogs].[CreditCardDepositRequestId]. The below is just a sample string formation. See the legacy sources for the exact algorhythm
             string requestID = DateTime.Now.ToString("HHmmddMMyy");
             string date = DateTime.Now.ToString("ddmmyy");
             string requestTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             string customerName = "Tony Stoyanov";
             string customerPhoneNumber = "359888123456";
-            string cardNumber = "4000000000001091";
-            string expiry = "122022";
-            string amount = "0.01";
+            string cardNumber = "4796874002989836";
+            string expiry = "112026";
+            string amount = "100";
             //Replace with PROfit's Deposit Site the 3DS Return URL, based on your environment (DEV2, QA2, ST, QAPROD, PROD)
             string redirectUrl = "https://tnstoyanov.wixsite.com/payment-response";
             //Replace with PROfit's Billing Server the callback handler URL, based on your environment (DEV2, QA2, ST, QAPROD, PROD)
             string timeoutUrl = "https://db46a064326442f75c6172ae6772049a.m.pipedream.net";
             string currency = "USD";
             //As per ISO Alpha-3
-            string country = "SAU";
-            string city = "KSA";
-            string cvv = "189";
+            string country = "BGR";
+            string city = "Sofia";
+            string cvv = "130";
             string postalCode = "1000";
             string stateCode = "";
             string email = "tony.stoyanov@tiebreak.solutions";
             string description = "Test Deposit";
+
+            string rawConcat = requestID + merchantID + amount + currency;
+            string checksum = GenerateSHA256(rawConcat);
 
             //This builds the request body in XML, no security checksum
             Console.WriteLine("<?xml version= \"1.0\" encoding= \"utf - 8\"?>");
@@ -52,10 +58,21 @@
             Console.WriteLine("<stateCode>" + stateCode + "</stateCode>");
             Console.WriteLine("<email>" + email + "</email>");
             Console.WriteLine("<description>" + description + "</description>");
+            Console.WriteLine("<checksum>" + checksum + "</checksum>");
             Console.WriteLine("</message>");
 
             //This keeps the console window open
             Console.ReadLine();
+
+
+            // SHA256 and Base64 hashing
+
+            static string GenerateSHA256(string value)
+            {
+                SHA256 sh = SHA256.Create();
+                byte[] hashValue = sh.ComputeHash(Encoding.UTF8.GetBytes(value));
+                return Convert.ToBase64String(hashValue);
+            }
         }
     }
 }
